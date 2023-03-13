6fd5f5e6782f02b82c143f477eeb6859e757d984
Pacey2706 <pacey2706@gmail.com>
2023-03-13 22:28:53 +0000

    add main functions

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def choose_word():
+    """
+    Chooses a word from random from the list
+    """
+    words = [blah, boop, numpty, pacey, arghhh]
+
+def player_guess():
+    """
+    returns players guess and checks validity
+    """
+
+def display_word():
+    """
+    returns word
+    """
+
+def main():
+    """runs main mechanics for hangman game"""
